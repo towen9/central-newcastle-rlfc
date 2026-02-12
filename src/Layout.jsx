2c80c22f399e508943b73a011feac545d09eddc5
t.owen <t.owen@nib.com.au>
2026-02-12 03:43:22 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children, currentPageName }) {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <script src="/register-sw.js" async></script>
       <PushNotificationManager />
       <style>{`
         :root {
